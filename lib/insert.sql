cc1114f30aad8006ae6e4455abc3e85581e2f09e
Edward <jaysongraphics@gmail.com>
2021-07-02 17:49:13 -0400

    commit tree da027039f9719175c18a4ec79646bfb875c3783f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 17, "M", "Blk", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 7, "M", "black", "intense", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 8, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 23, "M", "blk", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 21, "F", "brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 5, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 18, "F", "Blue", "naive", 1);
+INSERT INTO bears VALUES (8,null, 24, "M", "black", "aggressive", 0);
